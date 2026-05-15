54a6bce993d95e02456517f4eb3d18cd6eb48a48
Eslam Jobara <jobaraeslam@gmail.com>
2026-05-16 01:27:03 +0300

    chore: update application entry point, switch to ES modules, and add joi and nodemon dependencies

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("test");
 });
+
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log("test worked!");
 });
