c8139868646da1ece6f45413db1688b3a61c1dcd
Mohammed Ayman <m7md.ayman1993@gmail.com>
2026-05-22 02:26:29 +0300

    feat : create user model and auth controller and validation

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import globalError from "./middlewares/erorr.midlleware.js";
+import cors from 'cors';
+dotenv.config();
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+app.use(express.json());
+app.use(cookieParser());
+app.use('/auth', authRoute);
 app.get("/", (req, res) => {
   res.send("test");
 });
 
-app.listen(3000, () => {
+app.use(globalError)
+app.listen(process.env.PORT, () => {
   console.log("test worked!");
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("server is running");
+  })
