1d40762004e477a44e56503b100bdc5c5737c05b
Eslam Jobara <jobaraeslam@gmail.com>
2026-05-22 14:15:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://ostafinderuserfront.vercel.app/"],
     credentials: true,
   }),
 );
