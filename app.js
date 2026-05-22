117a4e20999d45be6cb9f4ad8ef81fbb4727c30f
Aml Gamal <amlgamal103@gmail.com>
2026-05-22 10:43:34 +0300

    (feat): CRUD category & global error

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 import express from "express";
+import mongoose from "mongoose"
+import "dotenv/config"
+
+import globalError from "./middlewares/error.middleware.js"
+import ApiError from "./utils/ApiError.js";
+import categoryRoutes from "./routes/category.route.js"
 
 const app = express();
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("test");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION);
+    console.log("DB connected successful");
+  } catch (err) {
+    console.log("DB connection Failed");
+    console.log(err.message);
+
+    process.exit(1)
+  }
+};
+
+await connectDB()
+
+// Routes
+app.use("/categories", categoryRoutes)
+
+//invalid route
+app.use((req, res, next)=>{
+  next(new ApiError(`Not Found : ${req.originalUrl}`, 404))
+})
+
+app.use(globalError);
+
+const port = process.env.PORT || 8000
+const server = app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
 });
 
-app.listen(3000, () => {
-  console.log("test worked!");
+process.on("unhandledRejection", (err) => {
+  console.log(`unhandledRejection Errors :${err.name} | ${err.message}`);
+  server.close(() => {
+    console.log(`shutting down..`);
+    process.exit(1);
+  });
 });
+
