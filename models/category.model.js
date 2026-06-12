98ea4125134fbad35e64dc1dee81b5be28cf443e
Aml Gamal <amlgamal103@gmail.com>
2026-06-12 03:06:39 +0300

    Emergency

--- models/category.model.js
+++ models/category.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const CategoySchema = new mongoose.Schema(
   {
@@ -9,7 +10,9 @@ const CategoySchema = new mongoose.Schema(
       unique: true,
       minlength: [2, "Category name must be at least 2 characters long"],
       maxlength: [50, "Category name cannot exceed 50 characters"],
+      index: true,
     },
+    slug: { type: String, lowercase: true },
     icon: {
       type: String,
       required: [true, "Category icon identifier or URL is required"],
@@ -24,5 +27,16 @@ const CategoySchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+CategoySchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("name")) {
+    this.slug = this.name
+      .trim()
+      .toLowerCase()
+      .replace(/[\s_]+/g, "-")
+      .replace(/[^\u0600-\u06FFa-z0-9-]+/g, "");
+  }
+  next();
+});
 
 export default mongoose.model("Category", CategoySchema);
